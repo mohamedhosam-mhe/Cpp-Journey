6a9862f5e930a1243854867572f8bb27b56e59e7
Mohamed Hosam <mhe.mohamedhosam@gmail.com>
2026-04-23 13:11:58 +0400

    starting in Algorithms & Problem solving level 3

---
+++ 7- Algorithms & Problem-Solving – Level 3/Problems 1/Problems 1.cpp
@@ -0,0 +1,29 @@
+﻿// Problems 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
